01f692a530aa947c6c92d802f3eecab3a8133f4e
Tiw Peter <tiwpeter@example.com>
2026-01-04 10:33:27 +0700

    chore(rebuild): remove legacy frontend and outdated files

--- fonted/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import 'tailwindcss/tailwind.css'
-import App from 'components/App'
-import './index.css'
-
-const container = document.getElementById('root') as HTMLDivElement
-const root = createRoot(container)
-
-root.render(<App />)
