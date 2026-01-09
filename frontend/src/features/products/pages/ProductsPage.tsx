e32186ef4c36c0794c222cd297985b2f27b6636b
Tiepeter777 <idkaimak01@gmail.com>
2026-01-09 20:07:23 +0700

    refactor(products): update ProductsPage to use TrackSearch component with enhanced UI

--- frontend/src/features/products/pages/ProductsPage.tsx
+++ frontend/src/features/products/pages/ProductsPage.tsx
@@ -1,10 +1,10 @@
 // src/features/products/pages/ProductsPage.jsx
+import ProductAddByUrl from "../pages/../components/ProductAddByUrl";
 
 export default function ProductsPage() {
-
-    return (
-        <>
-            products page
-        </>
-    );
+  return (
+    <>
+      <ProductAddByUrl />
+    </>
+  );
 }
