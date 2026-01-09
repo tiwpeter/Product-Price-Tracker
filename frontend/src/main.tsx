e8f92870d1be5da23ad6021e8eec9d9d98cfd108
Tiepeter777 <idkaimak01@gmail.com>
2026-01-09 17:29:42 +0700

    refactor: update app entry structure

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import ProductsPage from "./features/products/pages/ProductsPage";
+import MainLayout from "./components/Layout";
+
+export const router = createBrowserRouter([
+  {
+    element: <MainLayout />, // 👈 เรียก Layout ตรงนี้
+    children: [
+      {
+        path: "/",
+        element: <ProductsPage />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
